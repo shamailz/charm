50476717fbf3800b21dfc812a77a2a3b0538d602
Shamail <zahis538@newschool.edu>
2022-04-22 15:17:47 -0400

    Initial commit

---
+++ charm.js
@@ -0,0 +1,12 @@
+
+//debugging line :-D
+
+console.log("hello!");
+//popup!
+let info = "J Class";
+alert("Hi Charm! Here's my work!");
+
+let boxboy = document.createElement("h1");
+boxboy.innerHTML = info
+document.getElementById("container").appendChild(boxboy)
+
